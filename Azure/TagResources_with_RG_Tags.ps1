83d33120a0f8eb9e06a45a0112c0df4d1bfe649f
Djalil <abdeldjalil.sichaib@hotmail.fr>
2018-11-26 10:35:00 +0100

    Add files via upload

---
+++ Azure/TagResources_with_RG_Tags.ps1
@@ -0,0 +1,20 @@
+﻿Login-AzureRmAccount
+Select-AzureRmSubscription -SubscriptionId "f386634b-3dfd-47f8-af84-7fe9d01952f4"
+$groups = Get-AzureRmResourceGroup
+Write-Output $groups
+foreach ($g in $groups)
+{
+    if ($g.Tags -ne $null) {
+        $resources = Find-AzureRmResource -ResourceGroupNameEquals $g.ResourceGroupName
+        foreach ($r in $resources)
+        {
+            $resourcetags = (Get-AzureRmResource -ResourceId $r.ResourceId).Tags
+            foreach ($key in $g.Tags.Keys)
+            {
+                if ($resourcetags.ContainsKey($key)) { $resourcetags.Remove($key) }
+            }
+            $resourcetags += $g.Tags
+            Set-AzureRmResource -Tag $resourcetags -ResourceId $r.ResourceId -Force
+        }
+    }
+}
